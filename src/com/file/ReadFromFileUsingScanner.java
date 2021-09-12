4af00592ba13ed674a207ffa28d2f1bb6bb34b29
Satish Prasad <spd61185@gmail.com>
2021-09-12 10:27:40 +0530

    ReadFromFileUsingScanner

--- src/com/file/ReadFromFileUsingScanner.java
+++ src/com/file/ReadFromFileUsingScanner.java
@@ -1,5 +1,15 @@
 package com.file;
 
-public class ReadFromFileUsingScanner {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class ReadFromFileUsingScanner {
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File("E://File/test.txt");
+		Scanner sc = new Scanner(file);
+		while(sc.hasNext()) {
+			System.out.println(sc.nextLine());
+		}
+	}
 }
