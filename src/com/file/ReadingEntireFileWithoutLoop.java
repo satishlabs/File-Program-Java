b2c86a989e9a423c257870b46f291340a06833b7
Satish Prasad <spd61185@gmail.com>
2021-09-12 10:30:28 +0530

    Test

---
+++ src/com/file/ReadingEntireFileWithoutLoop.java
@@ -0,0 +1,14 @@
+package com.file;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadingEntireFileWithoutLoop {
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File("E://File/test.txt");
+		Scanner sc = new Scanner(file);
+		sc.useDelimiter("\\Z");
+		System.out.println(sc.next());
+	}
+}
